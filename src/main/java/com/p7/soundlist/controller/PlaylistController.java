fb4d912d2babfc05616cf3fe4865bf089dfa33b8
davidrtc14 <davidrtc14@gmail.com>
2026-05-30 16:34:08 -0300

    Endpoints para criação e busca páginada da playlists

--- src/main/java/com/p7/soundlist/controller/PlaylistController.java
+++ src/main/java/com/p7/soundlist/controller/PlaylistController.java
@@ -1,15 +1,30 @@
 package com.p7.soundlist.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.p7.soundlist.dtos.PlaylistRequestDto;
+import com.p7.soundlist.dtos.PlaylistResponseDto;
+import com.p7.soundlist.service.PlaylistService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/playlists")
+@RequiredArgsConstructor
 public class PlaylistController {
+    private final PlaylistService playlistService;
 
     @GetMapping
-    public String helloWorld(){
-        return "Hello World";
+    public ResponseEntity<Page<PlaylistResponseDto>> getAllPlaylists(Pageable pageable) {
+        var playlists = playlistService.getAll(pageable);
+        return ResponseEntity.ok(playlists);
+    }
+
+    @PostMapping
+    public ResponseEntity<PlaylistResponseDto> createPlaylist(@RequestBody @Valid PlaylistRequestDto playlistRequestDto) {
+        var createdPlaylist = playlistService.create(playlistRequestDto);
+        return ResponseEntity.ok(createdPlaylist);
     }
 }
