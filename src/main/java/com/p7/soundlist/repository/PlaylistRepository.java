255420d0a45035c6ef19f9ec2d9df19d7b66e04d
davidrtc14 <davidrtc14@gmail.com>
2026-05-30 15:05:05 -0300

    Acesso a tabela relacionada as playlists no BD

---
+++ src/main/java/com/p7/soundlist/repository/PlaylistRepository.java
@@ -0,0 +1,9 @@
+package com.p7.soundlist.repository;
+
+import com.p7.soundlist.model.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+}
