0ca4cbb35d9412bcf0389c98397c47f646e1c2c5
davidrtc14 <davidrtc14@gmail.com>
2026-05-31 11:33:11 -0300

    Adição das regras de negócio dos métodos http PUT, PATCH e DELETE

--- src/main/java/com/p7/soundlist/service/PlaylistService.java
+++ src/main/java/com/p7/soundlist/service/PlaylistService.java
@@ -1,8 +1,11 @@
 package com.p7.soundlist.service;
 
+import com.p7.soundlist.dtos.PlaylistPatchDto;
 import com.p7.soundlist.dtos.PlaylistRequestDto;
 import com.p7.soundlist.dtos.PlaylistResponseDto;
+import com.p7.soundlist.exception.BusinessException;
 import com.p7.soundlist.mapper.PlaylistMapper;
+import com.p7.soundlist.model.Playlist;
 import com.p7.soundlist.repository.PlaylistRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -34,5 +37,41 @@ public class PlaylistService {
         var playlist = mapper.toEntity(playlistRequestDto);
         var savedPlaylist = playlistRepository.save(playlist);
         return mapper.toDto(savedPlaylist);
+    }
+
+    //put
+    public PlaylistResponseDto update(Long id, PlaylistRequestDto playlistRequestDto){
+        Playlist playlist = playlistRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Playlist não encontrada"));
+
+        playlist.setName(playlistRequestDto.name());
+        playlist.setDescription(playlistRequestDto.description());
+
+        return mapper.toDto(playlistRepository.save(playlist));
+    }
+
+    //patch
+    public  PlaylistResponseDto patch(Long id, PlaylistPatchDto playlistPatchDto){
+        if (playlistPatchDto.name() == null && playlistPatchDto.description() == null) {
+            throw new BusinessException("Ao menos um campo deve ser informado para atualização");
+        }
+
+        if (playlistPatchDto.name() != null && playlistPatchDto.name().isBlank()) {
+            throw new BusinessException("Nome não pode ser vazio");
+        }
+
+        Playlist playlist = playlistRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Playlist não encontrada"));
+        if (playlistPatchDto.name() != null) {
+            playlist.setName(playlistPatchDto.name());
+        }
+        if (playlistPatchDto.description() != null) {
+            playlist.setDescription(playlistPatchDto.description());
+        }
+        return mapper.toDto(playlistRepository.save(playlist));
+    }
+
+    //delete
+    public void delete(Long id){
+        Playlist playlist = playlistRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Playlist não encontrada"));
+        playlistRepository.delete(playlist);
     }
 }
