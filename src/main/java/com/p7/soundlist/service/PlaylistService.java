fae47f85bb709456bb702f777ed4f8b1da48c902
davidrtc14 <davidrtc14@gmail.com>
2026-05-31 15:16:46 -0300

    Adição de Regras de negócio para selecionar todas as músicas de uma playlist e validar se o repository retornou essas músicas conforme esperado

--- src/main/java/com/p7/soundlist/service/PlaylistService.java
+++ src/main/java/com/p7/soundlist/service/PlaylistService.java
@@ -1,11 +1,14 @@
 package com.p7.soundlist.service;
 
+import com.p7.soundlist.dtos.MusicResponseDto;
 import com.p7.soundlist.dtos.PlaylistPatchDto;
 import com.p7.soundlist.dtos.PlaylistRequestDto;
 import com.p7.soundlist.dtos.PlaylistResponseDto;
 import com.p7.soundlist.exception.BusinessException;
+import com.p7.soundlist.mapper.MusicMapper;
 import com.p7.soundlist.mapper.PlaylistMapper;
 import com.p7.soundlist.model.Playlist;
+import com.p7.soundlist.repository.MusicRepository;
 import com.p7.soundlist.repository.PlaylistRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -13,30 +16,49 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class PlaylistService {
     private final PlaylistRepository playlistRepository;
-    private final PlaylistMapper mapper;
+    private final MusicRepository musicRepository;
+    private final MusicMapper musicMapper;
+    private final PlaylistMapper playlistMapper;
 
     //select * from playlists
     public Page<PlaylistResponseDto> getAll(Pageable pageable){
-        return playlistRepository.findAll(pageable).map(mapper::toDto);
+        var playlists = playlistRepository.findAll(pageable);
+
+        if(playlists.stream().count() == 0){
+            throw new EntityNotFoundException("Nenhuma playlist encontrada");
+        }
+
+        return playlists.map(playlistMapper::toDto);
     }
 
     //select * from playlists where id = ?
     public PlaylistResponseDto getById(Long id){
         var foundPlaylist = playlistRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Playlist não encontrada"));
-        return mapper.toDto(foundPlaylist);
+        return playlistMapper.toDto(foundPlaylist);
+    }
+
+    //select * from musics where playlistId = ?
+    public Page<MusicResponseDto> getMusicsByPlaylist(Long id, Pageable pageable) {
+        playlistRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Playlist não encontrada com id: " + id));
+
+        var playlistSongs = musicRepository.findByPlaylistId(id, pageable);
+        if(playlistSongs.stream().count() == 0){
+            throw new EntityNotFoundException("Nenhuma música encontrada para a playlist com id: " + id);
+        }
+
+        return playlistSongs.map(musicMapper::toDto);
     }
 
     //save
     public PlaylistResponseDto create(PlaylistRequestDto playlistRequestDto){
-        var playlist = mapper.toEntity(playlistRequestDto);
+        var playlist = playlistMapper.toEntity(playlistRequestDto);
         var savedPlaylist = playlistRepository.save(playlist);
-        return mapper.toDto(savedPlaylist);
+        return playlistMapper.toDto(savedPlaylist);
     }
 
     //put
@@ -46,7 +68,7 @@ public class PlaylistService {
         playlist.setName(playlistRequestDto.name());
         playlist.setDescription(playlistRequestDto.description());
 
-        return mapper.toDto(playlistRepository.save(playlist));
+        return playlistMapper.toDto(playlistRepository.save(playlist));
     }
 
     //patch
@@ -66,7 +88,7 @@ public class PlaylistService {
         if (playlistPatchDto.description() != null) {
             playlist.setDescription(playlistPatchDto.description());
         }
-        return mapper.toDto(playlistRepository.save(playlist));
+        return playlistMapper.toDto(playlistRepository.save(playlist));
     }
 
     //delete
