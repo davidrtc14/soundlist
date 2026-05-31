8e91f52bd95a99f22f48106293174887f8917daf
davidrtc14 <davidrtc14@gmail.com>
2026-05-31 15:19:58 -0300

    Acesso a tabela relacionada as músicas no BD

---
+++ src/main/java/com/p7/soundlist/repository/MusicRepository.java
@@ -0,0 +1,10 @@
+package com.p7.soundlist.repository;
+
+import com.p7.soundlist.model.Music;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MusicRepository extends JpaRepository<Music, Long> {
+    Page<Music> findByPlaylistId(Long playlistId, Pageable pageable);
+}
